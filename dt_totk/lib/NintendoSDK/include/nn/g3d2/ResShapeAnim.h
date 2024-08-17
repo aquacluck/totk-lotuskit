40daf96e993db013ba3f4109df720ed55615def8
aquacluck <aquacluck@proton.me>
2024-08-17 01:44:16 -0700

    [broken] patchless initial dt header import...
    base https://github.com/dt-12345/totk/commit/67e7145c8698f45ce0be6969bdb1184e1349bc21

---
+++ dt_totk/lib/NintendoSDK/include/nn/g3d2/ResShapeAnim.h
@@ -0,0 +1,15 @@
+/**
+ * @file ResShapeAnim.h
+ * @brief Resource file for shape animations.
+ */
+
+#pragma once
+
+namespace nn {
+namespace g3d2 {
+class ResShapeAnim {
+public:
+    void Reset();
+};
+}  // namespace g3d2
+}  // namespace nn
