42c8717f7b86f51fe6e73617788ab3e7526a65c0
aquacluck <aquacluck@proton.me>
2026-01-11 09:26:25 -0800

    misc stacktrace, add wip stackdump to trace

--- src/program/util/stack_trace.hpp
+++ src/program/util/stack_trace.hpp
@@ -4,6 +4,7 @@
 #include "cxxabi.h"
 #endif
 #include <cstring>
+#include "HexDump.hpp"
 // thanks dt-13269 for putting together initial impl
 
 namespace nn::diag::detail {
@@ -11,20 +12,8 @@ namespace nn::diag::detail {
 }
 
 namespace lotuskit::util::trace {
-    struct StackFrame {
-        uintptr_t fp;
-        uintptr_t lr;
-    };
-
-    struct AddressInfo {
-        const char* module_name;
-        ptrdiff_t offset = 0;
-        char nearest_sym_name[0x80];
-        ptrdiff_t nearest_sym_offset = 0;
-        //int module_index = -1;
-    };
-
-    void demangle(char* mangled_name, char* out_buffer, size_t out_size) {
+    /*
+    inline void demangle(char* mangled_name, char* out_buffer, size_t out_size) {
         #ifdef DEMANGLE
         int status;
         char* demangled = abi::__cxa_demangle(mangled_name, nullptr, 0, &status);
@@ -44,77 +33,88 @@ namespace lotuskit::util::trace {
         out_buffer[copy_size] = '\0';
         #endif
     }
+    */
 
-    void getAddressInfo(uintptr_t addr, AddressInfo& info) {
-        auto module = exl::util::TryGetModule(addr);
-        if (module == nullptr) {
-            info.module_name = nullptr;
-            return;
-        }
-        info.offset = addr - module->m_Total.m_Start;
-        info.module_name = module->GetModuleName().data();
-        //info.module_index = i;
+    class AddressInfo {
+        public:
+        uintptr_t addr = 0;
+        const char* module_name = nullptr;
+        ptrdiff_t offset = 0;
+        char nearest_sym_name[0x80];
+        ptrdiff_t nearest_sym_offset = 0;
 
-        u64 sym_size;
-        char const* sym_name;
-        uintptr_t sym_addr = nn::diag::detail::GetNearestExportedSymbol(&sym_name, &sym_size, addr);
-        if (sym_addr == 0 || addr - sym_addr >= sym_size) {
-            info.nearest_sym_name[0] = '\0';
-            info.nearest_sym_offset = 0;
-        } else {
-            info.nearest_sym_offset = addr - sym_addr;
-            size_t copy_size = strnlen(sym_name, sizeof(info.nearest_sym_name));
-            memcpy(info.nearest_sym_name, sym_name, copy_size);
-            info.nearest_sym_name[copy_size] = '\0';
+        inline void read(uintptr_t addr) {
+            this->addr = addr;
+            auto module = exl::util::TryGetModule(addr);
+            if (module == nullptr) {
+                module_name = nullptr;
+                return;
+            }
+            offset = addr - module->m_Total.m_Start;
+            module_name = module->GetModuleName().data();
+
+            u64 sym_size;
+            char const* sym_name;
+            uintptr_t sym_addr = nn::diag::detail::GetNearestExportedSymbol(&sym_name, &sym_size, addr);
+            if (sym_addr == 0 || addr - sym_addr >= sym_size) {
+                nearest_sym_name[0] = '\0';
+                nearest_sym_offset = 0;
+            } else {
+                nearest_sym_offset = addr - sym_addr;
+                size_t copy_size = strnlen(sym_name, sizeof(nearest_sym_name));
+                memcpy(nearest_sym_name, sym_name, copy_size);
+                nearest_sym_name[copy_size] = '\0';
+            }
         }
-    }
+    };
 
     template <size_t max_trace_size = 0x20>
     struct StackTrace {
         StackTrace() = default;
         size_t trace_size = 0;
         uintptr_t trace[max_trace_size];
-        uintptr_t getFramePtr() const { return reinterpret_cast<uintptr_t>(__builtin_frame_address(0)); }
 
         // this is essentially the atmosphere implementation
         // https://github.com/Atmosphere-NX/Atmosphere/blob/master/stratosphere/creport/source/creport_threads.cpp#L37
-        void unwind() {
-            uintptr_t current_fp = getFramePtr();
+        inline void unwind() {
+            auto* current_frame = exl::util::stack_trace::GetCurrentFrame();
+            uintptr_t current_fp = current_frame->m_Fp;
             trace_size = 0;
             for (size_t i = 0; i < max_trace_size; ++i) {
                 if (current_fp == 0 || current_fp % sizeof(void*) != 0) {
                     break;
                 }
-
-                const StackFrame* current_frame = reinterpret_cast<const StackFrame*>(current_fp);
-                if (current_frame == 0 || current_frame->lr == 0 || current_frame->lr % 4 != 0) {
+                current_frame = reinterpret_cast<exl::util::stack_trace::Frame*>(current_fp);
+                if (current_frame == 0 || current_frame->m_Lr == 0 || current_frame->m_Lr % 4 != 0) {
                     break;
                 }
-
-                trace[trace_size++] = current_frame->lr;
-                current_fp = current_frame->fp;
+                trace[trace_size++] = current_frame->m_Lr;
+                current_fp = current_frame->m_Fp;
             }
         }
 
-        void debugLog() {
+        inline void debugLog() {
             char buf[200];
             AddressInfo info;
             for (size_t i = 0; i < trace_size; ++i) {
-                uintptr_t addr = trace[i];
-                getAddressInfo(addr, info);
+                info.read(trace[i]);
                 if (info.module_name) {
                     if (info.nearest_sym_name[0] == '\0') {
-                        nn::util::SNPrintf(buf, sizeof(buf), "%p => %s+%08x", addr, info.module_name, info.offset);
+                        nn::util::SNPrintf(buf, sizeof(buf), "%p => %s+%08x", info.addr, info.module_name, info.offset);
                     } else {
-                        nn::util::SNPrintf(buf, sizeof(buf), "%p => %s+%08x [%s+%08x]", addr, info.module_name, info.offset, info.nearest_sym_name, info.nearest_sym_offset);
+                        nn::util::SNPrintf(buf, sizeof(buf), "%p => %s+%08x [%s+%08x]", info.addr, info.module_name, info.offset, info.nearest_sym_name, info.nearest_sym_offset);
                     }
                 } else {
-                    nn::util::SNPrintf(buf, sizeof(buf), "%p", addr);
+                    nn::util::SNPrintf(buf, sizeof(buf), "%p", info.addr);
                 }
                 svcOutputDebugString(buf, strlen(buf));
             }
-        }
 
+            auto sp = (void*)exl::util::stack_trace::GetSp();
+            nn::util::SNPrintf(buf, sizeof(buf), "stack dump (sp=%p):", sp);
+            svcOutputDebugString(buf, strlen(buf));
+            lotuskit::HexDump::print_text(-1, sp-0x40, sp-0x40, 8); // +-4 lines
+        }
     };
 
 } // namespace
