85b36630c37c06a8e0a63003894cfa41ec372ea6
aquacluck <aquacluck@proton.me>
2026-01-14 14:54:46 -0800

    add exefs_patches/lotuskit-standardallocator-shrink-2MB

--- exefs_patches/build.py
+++ exefs_patches/build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os, os.path, pathlib
+import os, os.path, sys, pathlib
 import struct
 
 class BuildId:
@@ -14,7 +14,7 @@ class BuildId:
 # IPS32 filename format is an uppercase nsobid string with ips extension. Contents:
 # 5B "IPS32" magic
 # patch_records[]:
-#     4B NSO offset (+0x100 from ghidra .text/MOD0)
+#     4B NSO offset (+0x100 above ghidra .text/MOD0)
 #     2B patch_size
 #     {patch_size}B patch_value
 # 4B "EEOF"
@@ -48,31 +48,39 @@ def b(v) -> bytes: # ez input
         return v
     if type(v) is int:
         v: str = hex(v)
-    v = str(v).lstrip("0x").replace(" ", "").replace("\t", "").replace("\r", "").replace("\n", "")
+    v = str(v).removeprefix("0x").replace(" ", "").replace("\t", "").replace("\r", "").replace("\n", "")
     if len(v) % 2:
         v = "0" + v # fromhex wants full bytes
     return bytes.fromhex(v)
 
-TOTK_IPS_PATCHSETS: PatchSetCollection = {
-    "nn-standardallocator-shrink-2MB": {
+LOTUSKIT_IPS_PATCHES: PatchSetCollection = {
+    "lotuskit-standardallocator-shrink-2MB": {
         BuildId.TOTK_100: {
-            0x01130f28: b("02 6c a8 52"), # w2,#0x43600000 # vanilla - 2MB
-            0x01130f2c: b("13 0e a0 52"), # w19,#0x700000  # vanilla - 2MB
-            0x01130f30: b("05 64 ae d2"), # x5,#0x73200000
-            #0x01130f44: b"\x04\x6c\xa8\xd2", # mov x4,#0x43600000
+            # StandardAllocator is 9MB -> 7MB
+            0x01130f38: b("00 0e a0 52"), # mov w0,#0x700000          // -2MB to engine::system::sStandardAllocatorMemBlockSize
+            0x01130f64: b("01 0e a0 d2"), # mov x1,#0x700000          // -2MB to nn::init::InitializeAllocator
+            0x01130f74: b("08 01 5c 91"), # add x8,x8,#0x700, LSL #12 // -2MB shift arena start
         },
         BuildId.TOTK_110: {
-            0x01160524: b"\x04\x64\xa8\xd2", # mov x4,#0x43200000
+            # StandardAllocator is 11MB -> 9MB (untested)
+            0x01160518: b("00 12 a0 52"), # mov w0,#0x900000
+            0x01160544: b("01 12 a0 d2"), # mov x1,#0x900000
+            0x01160554: b("08 01 64 91"), # add x8,x8,#0x900, LSL #12
         },
         BuildId.TOTK_121: {
-            0x01155ec4: b"\x04\x64\xa8\xd2", # mov x4,#0x43200000
+            # StandardAllocator is 11MB -> 9MB (untested)
+            0x01155eb8: b("00 12 a0 52"), # mov w0,#0x900000
+            0x01155ee4: b("01 12 a0 d2"), # mov x1,#0x900000
+            0x01155ef4: b("08 01 64 91"), # add x8,x8,#0x900, LSL #12
         },
     },
 }
 
 def main():
     os.chdir(os.path.dirname(__file__))
-    write_ips_patchsets(TOTK_IPS_PATCHSETS)
+    write_ips_patchsets(LOTUSKIT_IPS_PATCHES)
+    if "install" in sys.argv:
+        os.system("rsync -r lotuskit-* ~/appdata/Roaming/Ryujinx/sdcard/atmosphere/exefs_patches")
 
 if __name__ == "__main__":
     main()
